e720c8a62283600d0405f36b7f53b0b351be0858
Alex Peyrard <alex.peyrard@d2-si.eu>
2017-05-23 10:40:33 +0200

    initial commit

---
+++ case.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/v1/upper', methods=['POST'])
+def upper():
+    content = request.get_json()
+    if content is None:
+        text = ''
+    else:
+        try:
+            text = content['text'].upper()
+        except AttributeError:
+            text = ''
+    return jsonify({'text': text})
+
+
+@app.route('/v1/lower', methods=['POST'])
+def lower():
+    content = request.get_json()
+    if content is None:
+        text = ''
+    else:
+        try:
+            text = content['text'].lower()
+        except AttributeError:
+            text = ''
+    return jsonify({'text': text})
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
