f4d7dcc5b965e944e0002d04eee8841120f2787f
Alex Peyrard <alex.peyrard@d2-si.eu>
2017-05-29 14:51:02 +0200

    Add new endpoint and testto showcase versioning
    Change folder and import structure
    Add reverse endpoint

---
+++ Case/api/routes/v2/routes.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request, jsonify
+from ...utils import common
+
+api = Blueprint('v2', __name__)
+
+
+@api.route('/lower', methods=['POST'])
+def lower():
+    return common.lower()
+
+
+@api.route('/upper', methods=['POST'])
+def upper():
+    return common.upper()
+
+
+@api.route('/reverse', methods=['POST'])
+def reverse():
+    content = request.get_json()
+    if content is None:
+        text = ''
+    else:
+        try:
+            text = content['text'][::-1]
+        except AttributeError:
+            text = ''
+    return jsonify({'text': text})
--- tests/test_case.py
+++ tests/test_case.py
@@ -9,19 +9,3 @@ def test_health_check():
     response = app.get('/health-check')
     assert response.status_code == 200
     assert json.loads(response.data)["healthy"] is True
-
-
-def test_v1_lower():
-    response = app.post('/v1/lower',
-                        data='{"text": "Foo"}',
-                        headers={'Content-Type': 'application/json'})
-    assert response.status_code == 200
-    assert json.loads(response.data)["text"] == "foo"
-
-
-def test_v1_upper():
-    response = app.post('/v1/upper',
-                        data='{"text": "Foo"}',
-                        headers={'Content-Type': 'application/json'})
-    assert response.status_code == 200
-    assert json.loads(response.data)["text"] == "FOO"
