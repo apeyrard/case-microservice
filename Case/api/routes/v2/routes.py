4f6d2a479dbd5aec43813ecc4f9867795e105dd6
Alex Peyrard <alex.peyrard@d2-si.eu>
2017-06-01 14:25:15 +0200

    Add input expectation to routes

--- Case/api/routes/v2/routes.py
+++ Case/api/routes/v2/routes.py
@@ -1,25 +1,32 @@
 from flask import Blueprint, request
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, fields
 from ...utils import common
 
 blueprint = Blueprint('v2', __name__)
 api = Api(blueprint)
 
+text_payload = api.model('Resource', {
+    'text': fields.String,
+})
+
 
 @api.route('/lower')
 class Lower(Resource):
+    @api.expect(text_payload, validate=True)
     def post(self):
         return common.lower()
 
 
 @api.route('/upper')
 class Upper(Resource):
+    @api.expect(text_payload, validate=True)
     def post(self):
         return common.upper()
 
 
 @api.route('/reverse')
 class Reverse(Resource):
+    @api.expect(text_payload, validate=True)
     def post(self):
         content = request.get_json()
         if content is None:
